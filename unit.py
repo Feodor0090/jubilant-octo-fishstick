a4ad369942240f28b81e4d4c1668dea6b7a04584
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-09-27 11:24:26 +0300

    Add class

---
+++ unit.py
@@ -0,0 +1,16 @@
+class Unit:
+    def __init__(self, health, heat):
+        self.__health = health
+        self.__heat = heat
+
+    def health(self):
+        return self.__health
+
+    def __str__(self):
+        return f'Unit: {self.__health}'
+
+    def attack(self, other):
+        other.__health = other.__health - self.__heat
+
+    def is_dead(self):
+        return self.__health < 0
